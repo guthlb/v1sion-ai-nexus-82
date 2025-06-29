a382648a938329170ada37a24c97242b63a5310d
Ayati Rauthan <123970117+guthlb@users.noreply.github.com>
2025-06-29 15:03:12 +0530

    Remove navigation sections from hero page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,14 +1,6 @@
-
 import React from 'react';
 import Hero from '../components/Hero';
 import WhyAIFails from '../components/WhyAIFails';
-import MarketOpportunity from '../components/MarketOpportunity';
-import OurApproach from '../components/OurApproach';
-import ServiceOfferings from '../components/ServiceOfferings';
-import ProvenFramework from '../components/ProvenFramework';
-import Differentiators from '../components/Differentiators';
-import BusinessImpact from '../components/BusinessImpact';
-import ContactSection from '../components/ContactSection';
 import Navigation from '../components/Navigation';
 
 const Index = () => {
@@ -17,13 +9,6 @@ const Index = () => {
       <Navigation />
       <Hero />
       <WhyAIFails />
-      <MarketOpportunity />
-      <OurApproach />
-      <ServiceOfferings />
-      <ProvenFramework />
-      <Differentiators />
-      <BusinessImpact />
-      <ContactSection />
     </div>
   );
 };
